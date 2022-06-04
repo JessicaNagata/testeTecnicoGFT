b8924d23607b89c5e81b018a519ba9699d9fe560
JessicaNagata <jessica_thomaz@live.com>
2022-06-04 11:35:08 -0300

    Teste GIT

---
+++ testeTecnico_GFT/src/exercicio1/mainExercicio_1.java
@@ -0,0 +1,9 @@
+package exercicio1;
+
+public class mainExercicio_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ testeTecnico_GFT/src/exercicio2/mainExercicio_2.java
@@ -0,0 +1,9 @@
+package exercicio2;
+
+public class mainExercicio_2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
