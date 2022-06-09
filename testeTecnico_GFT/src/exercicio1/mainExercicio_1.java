b87d6808f6cbfc26dfa28f4bff69f99c705e67a2
JessicaNagata <jessica_thomaz@live.com>
2022-06-09 16:36:34 -0300

    Exercício 4 - Concluído

--- testeTecnico_GFT/src/exercicio1/mainExercicio_1.java
+++ testeTecnico_GFT/src/exercicio1/mainExercicio_1.java
@@ -1,9 +1,10 @@
 package exercicio1;
 
+import javax.swing.JOptionPane;
+
 public class mainExercicio_1 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
 }
--- testeTecnico_GFT/src/exercicio4/mainExercicio_4.java
+++ testeTecnico_GFT/src/exercicio4/mainExercicio_4.java
@@ -1,21 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio4;
 
-/**
- *
- * @author jessi
- */
+import javax.swing.JOptionPane;
+
 public class mainExercicio_4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String opt = "";
+
+        float nota1;
+        float nota2;
+        float nota3;
+        float peso1;
+        float peso2;
+        float peso3;
+
+        while (!opt.equals("3")) {
+            opt = JOptionPane.showInputDialog("Menu\n\n1 - Média aritmetica\n"
+                    + "2 - Média Ponderada\n"
+                    + "3 - Sair");
+
+            if (opt == null) {
+                System.exit(0);
+            }
+            switch (opt) {
+                case "1":
+                    nota1 = Float.parseFloat(JOptionPane.showInputDialog("Nota 1: "));
+                    nota2 = Float.parseFloat(JOptionPane.showInputDialog("Nota 2: "));
+
+                    JOptionPane.showMessageDialog(null, "Média Aritmética: " + (nota1 + nota2) / 2);
+
+                    break;
+
+                case "2":
+                    nota1 = Float.parseFloat(JOptionPane.showInputDialog("Nota 1: "));
+                    peso1 = Float.parseFloat(JOptionPane.showInputDialog("Peso nota 1: "));
+                    nota1 = nota1 * peso1;
+
+                    nota2 = Float.parseFloat(JOptionPane.showInputDialog("Nota 2: "));
+                    peso2 = Float.parseFloat(JOptionPane.showInputDialog("Peso nota 2: "));
+                    nota2 = nota2 * peso2;
+
+                    nota3 = Float.parseFloat(JOptionPane.showInputDialog("Nota 3: "));
+                    peso3 = Float.parseFloat(JOptionPane.showInputDialog("Peso nota 3: "));
+                    nota3 = nota3 * peso3;
+
+                    JOptionPane.showMessageDialog(null, "Média Ponderada: " + (nota1 + nota2 + nota3) / (peso1 + peso2 + peso3));
+
+                    break;
+
+                case "3":
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida, digite novamente", "Atenção", JOptionPane.ERROR_MESSAGE);
+                    break;
+            }
+        }
     }
-    
 }
