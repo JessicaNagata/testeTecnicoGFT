849d5766db91dcbe516ddcde9b1160b85bb5b8c8
JessicaNagata <jessica_thomaz@live.com>
2022-06-09 17:08:56 -0300

    Exercício 2 - Concluído

--- testeTecnico_GFT/src/exercicio2/mainExercicio_2.java
+++ testeTecnico_GFT/src/exercicio2/mainExercicio_2.java
@@ -1,9 +1,43 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class mainExercicio_2 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        float a;
+        float b;
+        float c;
+        double delta; 
+        double x1; 
+        double x2;
+        
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Valor de a: ");
+        a = ler.nextFloat();
+        
+        System.out.println("Valor de b: ");
+        b = ler.nextFloat();
+        
+        System.out.println("Valor de c: ");
+        c = ler.nextFloat();
+
+        delta = ((b * b)) - ((4 * a) * c);
+        
+        System.out.println(delta);
+
+        if (delta >= 0) {
+            x1 = (double) ((-(b) + Math.sqrt(delta)) / 2 * a);
+            x2 = (double) ((-(b) - Math.sqrt(delta)) / 2 * a);
+            
+            System.out.println("x': " + x1 + "\nx' ':"  + x2 );
+            
+        } else {
+            System.out.println("Delta não possui raiz ou ela é negativa, o que não permite o cálculo de x' e x' '");
+            System.exit(0);
+        }
+
     }
-    
 }
